7a8baac56240cf53e63f899e4df7ac16a873a9cf
Xavi Ivars <xavi.ivars@gmail.com>
2015-09-27 14:56:14 +0200

    Integrating Types with Timber

---
+++ types-timber-integration.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Plugin Name: Types Timber Integration
+Plugin URI: http://timber.upstatement.com
+Description: Integrates the Timber (Twig) template engine with the Types plugin
+Version: 0.1
+Author: Xavi Ivars xavi.ivars@gmail.com
+Author URI: http://xavi.ivars.me
+License: GPLv3
+GitHub Plugin URI: xavivars/types-timber-integration
+*/
+
+class WpTypesTimber {
+
+	function __construct() {
+		add_filter( 'timber_post_get_meta', array( $this, 'post_get_meta' ), 10, 2 );
+		add_filter( 'timber_post_get_meta_field', array( $this, 'post_get_meta_field' ), 10, 3 );
+	}
+
+	function post_get_meta( $customs ) {
+		foreach($customs as $key=>$value){
+			$no_wpcf_key = str_replace('wpcf-', '', $key);
+			$customs[$no_wpcf_key] = $value;
+		}
+
+		return $customs;
+	}
+	
+	function post_get_meta_field( $value, $post_id, $field_name ) {
+		return types_child_posts('links', $post_id);
+	}
+}
+ 
+function types_timber_integration_init() {
+	if ( defined( 'WPCF_META_PREFIX' ) && class_exists( 'Timber' )) {
+			new WpTypesTimber();
+	}
+}
+
+add_action( 'plugins_loaded', 'types_timber_integration_init' );
